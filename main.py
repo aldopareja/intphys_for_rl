reduced epsilon for the covariance matrix, made the mixture probabilities positive (removing a bug) and added a way to sample from a batched multivariate normal. I will now work on improving the batching since only a single theta get's output.

main.py
@@ -23,7 +23,7 @@ def solve_ode_sample(batch_size, n_vars, y_init_range, theta_range, noise_std_ra
     ts = ts.to(device)
 
     y_mean = odeint(lambda t, y: dy_dt(y, theta=theta), y_init, ts, rtol=1e-6, atol=1e-5)
-    y_obs = torch.distributions.Normal(loc=y_mean, scale=noise_std_ratio * torch.abs(y_mean)).rsample()
+    y_obs = torch.distributions.Normal(loc=y_mean, scale=noise_std_ratio * torch.abs(y_mean) + 1e-5).rsample()
     y_obs = y_obs.swapaxes(0, 1)
 
     return theta, y_obs
@@ -64,6 +64,14 @@ class BatchedMultivariateNormalMixture():
         category_log_prob = torch.log(self.mixture_probs)
 
         return torch.logsumexp(normal_log_prob + category_log_prob, dim=1)
+
+    def sample(self, num_samples):
+        sample = self.multivariatenormals.sample((num_samples,))
+        cat = torch.distributions.Categorical(self.mixture_probs)
+        cat_sample = cat.sample((num_samples,)).squeeze()
+        return sample[torch.arange(sample.shape[0]),
+                      torch.arange(sample.shape[1]),
+                      cat_sample]
 
 
 class SeqGaussMixPosterior(nn.Module):
@@ -236,7 +244,7 @@ class GaussianMixturePosterior(nn.Module):
         :return:
         '''
         output = self.dist_decoder(seqembed)
-        mix_p = output[:, :, 0]
+        mix_p = torch.exp(output[:, :, 0])
         means = output[:, :, 1:1 + self.num_means]
         covariance_terms = output[:, :, 1 + self.num_means:]
         covariance_matrices = self.get_covariance_matrices_from_vectors(covariance_terms,
@@ -247,7 +255,7 @@ class GaussianMixturePosterior(nn.Module):
                                                 covariance_matrices=covariance_matrices)
 
     @staticmethod
-    def get_covariance_matrices_from_vectors(covariance_terms, device, eps=0.01):
+    def get_covariance_matrices_from_vectors(covariance_terms, device, eps=0.001):
         '''
         :param covariance_terms: B x num_mixtures x n(n+1)/2 terms on the lower triangular matrices used to compute the covariance matrices
         :param device:

test_inferenceNormalMixture.py
@@ -8,22 +8,29 @@ n_vars = 2
 batch_size = 1000
 d_model = 60
 noise_std_ratio = 0.1
+num_trasformer_layers = 2
 
 obs_to_embed = u.MLP(input_size=n_vars,output_size=d_model,num_hidden_layers=1,num_hidden_units=d_model*2, unsqueeze=False)
 
 
 obs_to_embed = m.SeqPosteriorTransformer(obs_embedder=obs_to_embed,
                                          d_model=d_model,
-                                         num_transformer_layers=1,
+                                         num_transformer_layers=num_trasformer_layers,
                                          positional_encoding=True,
-                                         decode_length=1)
+                                         decode_length=2)
 
 inference_network = m.GaussianMixturePosterior(obs_to_embed= obs_to_embed,
                                                num_latents= n_vars**2).cuda()
 
-optimizer = torch.optim.Adam(params=inference_network.parameters())
+optimizer = torch.optim.Adam(params=inference_network.parameters(), lr=0.00001)
 
 gen_traces_fn = lambda: m.solve_ode_sample(batch_size, n_vars, [-10.0, 10.0], [-0.1, 0.1], noise_std_ratio, ts,
                                           device='cuda')
 
-losses = m.amortize_inference(inference_network, gen_traces_fn, optimizer, 2000)
+losses = m.amortize_inference(inference_network, gen_traces_fn, optimizer, 100)
+
+theta, sample = m.solve_ode_sample(2, n_vars, [-10.0, 10.0], [-0.1, 0.1], noise_std_ratio, ts,
+                                       device='cuda')
+
+posterior = inference_network.get_q_x_given_seqembed(inference_network.obs_to_embed(sample[:1]))
+posterior.sample(10)